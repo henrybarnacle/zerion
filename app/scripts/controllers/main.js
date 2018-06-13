6124474d647e33bf2e1d4c8ce57b53b0306ca124
Henry Barnacle <henrybarnacle@yahoo.com>
2018-06-13 15:42:21 -0400

    cleaning up code

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,53 +1,51 @@
 'use strict';
 
+const accessToken = "aa2a64dcb3b3da94a38003863fabcbbb44b10e70-5abbf73c06fd7a0b03a07bfd1cbe71a9a1a7102e";
 
 
 angular.module('routeApp').
-controller('homeCtrl' , function ($rootScope, $scope, $http) {
+controller('homeCtrl', function($rootScope, $scope, $http) {
     this.title = "This is the homepage";
-    this.accessToken = "aa2a64dcb3b3da94a38003863fabcbbb44b10e70-5abbf73c06fd7a0b03a07bfd1cbe71a9a1a7102e";
 
     // GET ALL ENTRIES FOR MAIN PAGE ///
 
-        $scope.getEntries = function () {
-			$http({
-				
-			  method: 'GET',
-			 	url: 'https://alpha-dataflownode.zerionsoftware.com/code_assignment/records',
-		   	  headers: 
-		         	{ 
-		        	  "Authorization": "Bearer aa2a64dcb3b3da94a38003863fabcbbb44b10e70-5abbf73c06fd7a0b03a07bfd1cbe71a9a1a7102e"
-		        	}
-
-			}).then(function successCallback(response) {
-			   console.log(response);
-			   $scope.entries = response.data;
-			  }, function errorCallback(response) {
-			    // called asynchronously if an error occurs
-			    // or server returns response with an error status.
-			  })
-			}
-
-			/// GET SINGLE ENTRY ON CLICK AND SHOW IN FORM FOR EDITING / DELETE ///
-
-		$scope.getEntry = function(id) {
+    $scope.getEntries = function() {
         $http({
-		   method: 'GET',
-		   url:"https://alpha-dataflownode.zerionsoftware.com/code_assignment/records/" + id,
-		   headers: 
-		         { 
-		           "Authorization": "Bearer aa2a64dcb3b3da94a38003863fabcbbb44b10e70-5abbf73c06fd7a0b03a07bfd1cbe71a9a1a7102e"
-		         }
 
-			}).then(function successCallback(response) {
-			   console.log(response);
-			   $scope.sampleFunction(response) ;
+            method: 'GET',
+            url: 'https://alpha-dataflownode.zerionsoftware.com/code_assignment/records',
+            headers: {
+                "Authorization": "Bearer " + accessToken,
+            }
 
-			   
-			  }, function errorCallback(response) {
+        }).then(function successCallback(response) {
+            console.log(response);
+            $scope.entries = response.data;
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        })
+    }
 
-			  })
-			}
+    /// GET SINGLE ENTRY ON CLICK AND SHOW IN FORM FOR EDITING / DELETE ///
+
+    $scope.getEntry = function(id) {
+        $http({
+            method: 'GET',
+            url: "https://alpha-dataflownode.zerionsoftware.com/code_assignment/records/" + id,
+            headers: {
+                "Authorization": "Bearer " + accessToken
+            }
+
+        }).then(function successCallback(response) {
+            console.log(response);
+            $scope.sampleFunction(response);
+
+
+        }, function errorCallback(response) {
+
+        })
+    }
 
 });
 
